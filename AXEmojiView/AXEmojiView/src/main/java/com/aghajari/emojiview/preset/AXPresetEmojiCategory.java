da455a3ccb9e9670acf5c376186ecd3ac7d9e666
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-03 00:19:03 +0800

    generateSQLite

--- AXEmojiView/AXEmojiView/src/main/java/com/aghajari/emojiview/preset/AXPresetEmojiCategory.java
+++ AXEmojiView/AXEmojiView/src/main/java/com/aghajari/emojiview/preset/AXPresetEmojiCategory.java
@@ -1,5 +1,6 @@
 package com.aghajari.emojiview.preset;
 
+import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -7,15 +8,22 @@ import androidx.annotation.NonNull;
 import com.aghajari.emojiview.emoji.Emoji;
 import com.aghajari.emojiview.emoji.EmojiCategory;
 import com.aghajari.emojiview.emoji.EmojiData;
+import com.aghajari.emojiview.repository.EmojiDao;
+import com.aghajari.emojiview.repository.EmojiRoomDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AXPresetEmojiCategory implements EmojiCategory {
+    private final Context context;
+
     public Emoji[] emojiData;
     public String title;
     int icon;
 
-    public AXPresetEmojiCategory(int i, int icon, EmojiData emojiData) {
+    public AXPresetEmojiCategory(Context context, int i, int icon, EmojiData emojiData) {
+        this.context = context;
+
         String[][] dataColored = emojiData.getDataColored();
         this.emojiData = new Emoji[dataColored[i].length];
         for (int j = 0; j < dataColored[i].length; j++)
@@ -34,21 +42,40 @@ public class AXPresetEmojiCategory implements EmojiCategory {
         category
         selected
          */
-        if (category == 1) {
-            for (Emoji emoji : this.emojiData) {
-                List<Emoji> variants = emoji.getVariants();
-                if (variants.isEmpty()) {
-                    Log.i("CHEOK", emoji.getUnicode() + "," + "," + (category) + "," + true);
-                } else {
-                    Log.i("CHEOK", emoji.getUnicode() + "," + emoji.getUnicode() + "," + (category) + "," + true);
-                    for (Emoji v : variants) {
-                        Log.i("CHEOK", v.getUnicode() + "," + emoji.getUnicode() + "," + (category) + "," + false);
-                    }
+
+        List<com.aghajari.emojiview.model.Emoji> emojis = new ArrayList<>();
+
+        for (Emoji emoji : this.emojiData) {
+            com.aghajari.emojiview.model.Emoji emojiModel = new com.aghajari.emojiview.model.Emoji();
+            emojiModel.unicode = emoji.getUnicode();
+            emojiModel.category = category;
+
+            List<Emoji> variants = emoji.getVariants();
+            if (variants.isEmpty()) {
+                emojiModel.group = null;
+                emojiModel.selected = true;
+                emojis.add(emojiModel);
+            } else {
+                emojiModel.group = emoji.getUnicode();
+                emojiModel.selected = true;
+                emojis.add(emojiModel);
+
+                for (Emoji v : variants) {
+                    emojiModel = new com.aghajari.emojiview.model.Emoji();
+                    emojiModel.unicode = v.getUnicode();
+                    emojiModel.category = category;
+                    emojiModel.group = emoji.getUnicode();
+                    emojiModel.selected = false;
+                    emojis.add(emojiModel);
                 }
             }
-        }    
+        }
+
+        EmojiDao emojiDao = EmojiRoomDatabase.instance(context).emojiDao();
+        emojiDao.insertAll(emojis);
     }
-    
+
+
     protected Emoji createEmoji(String code, int category, int index, EmojiData emojiData){
         return new AXPresetEmoji(code, emojiData);
     }
